8286731c665abc0d2c94c8047df2623b78b9e652
mccd326 <mccd326hd@pm.me>
2025-06-11 19:52:29 +0200

    First version

---
+++ dlljbltest/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "lib.h"
+
+int main(int argc, char** args){
+    Startup();
+
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
+
+
+TEST(SomeTest, MyTest)
+{
+    std::string myString = "Hello";
+    EXPECT_EQ(myString, "World!");
+}
