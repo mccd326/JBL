e26f704b7054da53695378607d9cf822173c15ee
mccd326 <mccd326hd@pm.me>
2025-06-12 21:35:54 +0200

    Seemingly working compilation with xp debug preset

---
+++ legacyTest/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "lib.h"
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+    char MyBuffer[20];
+    size_t someSize = 0;
+    std::string architecture;
+    if(sizeof(size_t) == 4){
+        architecture = "32 bit";
+    }
+    else{
+        architecture = "64 bit";
+    }
+    std::cout << "We are currently running on : " << architecture << std::endl;
+
+    Startup();
+}
