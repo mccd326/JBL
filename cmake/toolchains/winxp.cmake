1b55c3defdf37cbe171df0782e574c8c2a28539b
MCCD326 <mccd326hd@pm.me>
2025-06-12 21:35:54 +0200

    Seemingly working compilation with xp debug preset

--- cmake/toolchains/winxp.cmake
+++ cmake/toolchains/winxp.cmake
@@ -2,9 +2,9 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_VERSION 5.1) # Windows XP
 
 set(CMAKE_GENERATOR "MinGW MakeFiles")
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER windres)
+set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 add_compile_definitions(_WIN32_WINNT=0x0501)
 set(CMAKE_C_STANDARD 11)
