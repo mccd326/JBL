c40886c062e20b6f5564030ead974e8359d01f1b
mccd326 <mccd326hd@pm.me>
2025-06-19 15:24:34 +0200

    New defines for active os platform OS information method for library

--- legacyTest/src/main.cpp
+++ legacyTest/src/main.cpp
@@ -12,8 +12,11 @@ int main(){
         architecture = "64 bit";
     }
     std::cout << "We are currently running on : " << architecture << std::endl;
-
+#if defined(JBL_WINDOWs)
     std::cout << "Defined Windows Version " << _WIN32_WINNT << std::endl;
+#elif defined(JBL_LINUX)
+    std::cout << "Defined OS: Linux" << std::endl;
+#endif
 
-    Startup();
+    GetSystemInformation();
 }
