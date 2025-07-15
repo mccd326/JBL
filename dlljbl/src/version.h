bdf9cba06777ba05e2d75f8345b80b01af323ce9
mccd326 <mccd326hd@pm.me>
2025-07-15 19:07:13 +0200

    Fixed path to legacy test build directory in MinGW pipeline. Renamed MinGW workflow file

--- dlljbl/src/version.h
+++ dlljbl/src/version.h
@@ -4,6 +4,7 @@
  2025-07-14     0.0.1.0     Initial version, creation of version.h and versioning macros.
  2025-07-15     0.0.1.1     Fix for modern Windows toolchain, which didn't define _WIN32_WINNT.
                             Split Windows pipeline into two separate workflows, one for MSVC and one for MinGW.
+ 2025-07-15     0.0.1.2     Fixed path to legacy test build directory in MinGW pipeline.
 */
 
 #define JBL_STRINGIFY(x) #x
@@ -12,7 +13,7 @@
 #define JBL_VERSION_MAJOR    0
 #define JBL_VERSION_MINOR    0
 #define JBL_VERSION_PATCH    1
-#define JBL_VERSION_BUILD    1
+#define JBL_VERSION_BUILD    2
 #define JBL_VERSION_STRING   JBL_STR(JBL_VERSION_MAJOR) "." \
                             JBL_STR(JBL_VERSION_MINOR) "." \
                             JBL_STR(JBL_VERSION_PATCH) "." \
